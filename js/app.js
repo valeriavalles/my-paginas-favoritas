bdfcd2f53fa6a2f85f05f3ee18ebd28e3cb835e3
valeriavalles <valeria.sv@outlook.es>
2017-12-10 16:51:17 -0500

    subiendo responsive htm/css/js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+var botonMenu = document.getElementById('boton-menu');
+var nav = document.getElementById('nav');
+
+botonMenu.addEventListener('click',function(){
+    nav.classList.toggle('mostrar')
+})
